09fc95c53dffea09e716fb147052e21891648a8f
gosher <macciak@gmail.com>
2017-09-22 12:42:31 +0000

    Add edit and destroy actions for Reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :find_play
+  before_action :find_review, only: [:edit,:update,:destroy]
 
 def new
     @review = Review.new
@@ -18,6 +19,22 @@ def create
     end
 end
 
+def edit
+end
+
+def update
+  if @review.update(review_params)
+    redirect_to play_path(@play)
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+  @review.destroy
+  redirect_to play_path(@play)
+end
+
 private
   def review_params
     params.require(:review).permit(:rating, :comment)
@@ -25,6 +42,10 @@ private
 
   def find_play
     @play = Play.find(params[:play_id])
+  end
+  
+  def find_review
+    @review = Review.find(params[:id])
   end
 
 end
